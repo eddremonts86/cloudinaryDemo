913cdd8642247c2c1556463a19474045ecdbee60
Eduardo Inerarte <Eduardo.Inerarte@yrbrands.com>
2020-04-22 12:54:21 +0200

    demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container" style="width: 80%; padding: 40px; margin: 50px;">
+    <div class="card">
+      <div class="card-header">Claudinary Integration</div>
+      <div class="card-body">
+        <div class="col-sm-6" style="float: left;">
+          <form>
+            <div class="form-group">
+              <label for="Font">Font</label>
+              <select class="form-control" id="font" v-model="font">
+                <option v-for="i in fontArray" :key="i">
+                  {{ i }}
+                </option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="Font-size">Font size</label>
+              <select class="form-control" id="fontSize" v-model="fontSize">
+                <option v-for="i in fontSizeArray" :key="i">
+                  {{ i }}
+                </option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="Font-weight">Font weight</label>
+              <select class="form-control" id="fontWeight" v-model="fontWeight">
+                <option v-for="i in fontWeightArray" :key="i">
+                  {{ i }}
+                </option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="Text">Text</label>
+              <textarea
+                class="form-control"
+                id="Text"
+                rows="3"
+                v-model="text"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+        <div class="col-sm-6" style="float: right;">
+          <img width="100%" height="350px" :src="imgen" />
+          <div style="margin-top:25px">URL : {{ imgen }} </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "cloudinar",
+  data: () => {
+    return {
+      font: "Arial",
+      fontSize: "100",
+      fontWeight: "100",
+      text: "OTD&Cloudinary integration",
+      cloudynaryIMGURL: "https://res.cloudinary.com/demo/image/upload/l_text:",
+      fontWeightArray: [
+        "100",
+        "200",
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800",
+        "900",
+        "bold",
+        "bolder",
+      ],
+      fontSizeArray: [
+        "100",
+        "200",
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800",
+        "900",
+      ],
+      fontArray: [
+        "Arial",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Noto Sans",
+        "sans - serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    };
+  },
+  computed: {
+    imgen: function() {
+      var img =
+        this.cloudynaryIMGURL +
+        this.font +
+        "_" +
+        this.fontSize +
+        "_" +
+        this.fontWeight +
+        ":" +
+        this.text +
+        "/photo-1430609098125-581618d0482f_xvayby";
+      return img;
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
